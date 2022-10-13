d7153367abdd36fefc6752545944d781d972255d
igor.kornelli <aztecsrv@gmail.com>
2022-10-13 13:44:38 +0300

    add: translate to page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,19 +2,23 @@ import { GetServerSideProps } from "next";
 import { Locales } from "@/i18n/i18n-types";
 import { Button } from "ui";
 import { loadLocaleAsync } from "@/i18n/i18n-util.async";
-import { loadedLocales } from "@/i18n/i18n-util";
+import {baseLocale, loadedLocales} from "@/i18n/i18n-util";
+import {useContext} from "react";
+import {I18nContext} from "@/i18n/i18n-react";
 
 export default function Web() {
+  const { locale, LL, setLocale } = useContext(I18nContext);
   return (
     <div>
       <h1>Web</h1>
+      {LL.HI({name: "project"})}
       <Button />
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const locale = context.locale as Locales;
+  const locale = context.locale as Locales || baseLocale;
   await loadLocaleAsync(locale);
 
   return {
